57970fa884b034eb96342cfb73778d4e1fcfde52
Yannick Gbaguidi <gyannickange@gmail.com>
2026-03-30 00:24:20 +0100

    feat: disable Facebook OAuth and migrate Google/Facebook credentials to Rails encrypted credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,10 +272,16 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   require "omniauth-google-oauth2"
-  config.omniauth :google_oauth2, ENV["GOOGLE_AUTH_CLIENT_ID"], ENV["GOOGLE_AUTH_CLIENT_SECRET"], {}
+  config.omniauth :google_oauth2,
+                  Rails.application.credentials.dig(:google, :client_id),
+                  Rails.application.credentials.dig(:google, :client_secret),
+                  {}
 
-  require "omniauth-facebook"
-  config.omniauth :facebook, ENV["FACEBOOK_AUTH_CLIENT_ID"], ENV["FACEBOOK_AUTH_CLIENT_SECRET"], {}
+  # require "omniauth-facebook"
+  # config.omniauth :facebook,
+  #                 Rails.application.credentials.dig(:facebook, :client_id),
+  #                 Rails.application.credentials.dig(:facebook, :client_secret),
+  #                 {}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
